6be8e4d460c6bfcc6747c954ae06eca98adccedf
Javier <ibrahimcruz06@gmail.com>
2023-07-01 16:11:45 -0600

    Lectura de datos desde el archivo externo

--- ACP.py
+++ ACP.py
@@ -1,1 +1,15 @@
-class ACP: 
+#Tarea Programada Algebra Lineal 
+#Javier Cruz C02517 Alexander Wang Wu C28559 Sebastián Arce Flores C10577
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import scipy.linalg as la 
+import pandas as pd 
+import seaborn as sns 
+
+#Cargar los datos del archivo EjemploEdtudiantes.csv
+
+datos = pd.read_csv('EjemploEstudiantes.csv', delimiter=';', decimal= ",", index_col= 0) 
+ 
+
+print(datos)
